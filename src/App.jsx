5c83ca0b526d5b6026331ac43d645bd48593c5c6
yousrf391 <yousefouddane3@gmail.com>
2024-05-04 22:59:01 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import Porfile from "./assets/yousef.jpg";
-import Project1 from "./assets/project1.png";
-import Project2 from "./assets/project2.png";
-import Project3 from "./assets/project3.png";
-import Project4 from "./assets/project4.png";
+import Project1 from "./assets/stockeep.png";
+import Project2 from "./assets/hiking.png";
+import Project3 from "./assets/fitted.png";
+import Project4 from "./assets/BLoodAPP.png";
 import Facebook from "./assets/facebook.svg";
 import LinkedIn from "./assets/linkedin.svg";
 import Instagram from "./assets/instagram.svg";
@@ -14,43 +14,52 @@ function App() {
   const [scrolling, setScrolling] = useState(false);
 
   const onPageScroll = () => {
-    if(window.pageYOffset > 200) {
-      setScrolling(true)
+    if (window.pageYOffset > 200) {
+      setScrolling(true);
     } else {
       setScrolling(false);
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener("scroll", onPageScroll)
-    return() => {
-      window.removeEventListener("scroll", onPageScroll)
-    }
-  }, [])
+    window.addEventListener("scroll", onPageScroll);
+    return () => {
+      window.removeEventListener("scroll", onPageScroll);
+    };
+  }, []);
 
   return (
     <div className="max-w-4xl m-auto relative">
-      <header className={`${scrolling ? 'border-b border-gray-900' : ''}  fixed left-0 right-0 top-0 z-20`} id="home">
+      <header
+        className={`${
+          scrolling ? "border-b border-gray-900" : ""
+        }  fixed left-0 right-0 top-0 z-20`}
+        id="home"
+      >
         <div className="container m-auto px-4 py-6 max-w-4xl bg-black">
           <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
             <div>
-              <h1 className="font-bold text-2xl">Youcef Portfolio</h1>
+              <h1 className="font-bold text-2xl">Portfolio</h1>
             </div>
             <div>
               <ul className="flex gap-4">
                 <li>
-                  <a href="#projects" className="text-gray-400 hover:text-white cursor-pointer">
+                  <a
+                    href="#projects"
+                    className="text-gray-400 hover:text-white cursor-pointer"
+                  >
                     Projects
                   </a>
                 </li>
                 <li>
-                  <a href="#technologies" className="text-gray-400 hover:text-white cursor-pointer">
+                  <a
+                    href="#technologies"
+                    className="text-gray-400 hover:text-white cursor-pointer"
+                  >
                     Technologies
                   </a>
                 </li>
-                <li>
-                  
-                </li>
+                <li></li>
               </ul>
             </div>
           </div>
@@ -61,21 +70,26 @@ function App() {
         <section>
           <div className="container m-auto px-4 pt-12 pb-12 sm:pt-20 flex flex-col sm:flex-row gap-6 text-center sm:text-left">
             <div>
-              <h2 className="font-bold text-4xl">Hello, I'm  youcef,</h2>
+              <h2 className="font-bold text-4xl">Hello, I'm youcef,</h2>
               <div>
-                <h2 className="font-bold text-4xl mt-1 gradiant-text">Mobile developer</h2>
+                <h2 className="font-bold text-4xl mt-1 gradiant-text">
+                  Mobile developer
+                </h2>
               </div>
               <div>
                 <p className="mt-4 text-gray-400">
-                Experienced in both Flutter and Django,
-                 I specialize in building sleek frontends and robust backends for seamless app experiences.
+                  Experienced in both Flutter and Django, I specialize in
+                  building sleek frontends and robust backends for seamless app
+                  experiences.
                 </p>
-              
               </div>
             </div>
             <div className="relative">
               <div className="after:bg-[url('./large-long.png')] after:bg-contain after:block after:bg-no-repeat after:w-[420px] after:h-[320px] after:absolute after:top-0 after:-left-20 sm:after:-left-40 before:bg-[url('./small.png')] before:bg-contain before:block before:bg-no-repeat before:w-[220px] before:h-[220px] before:absolute before:bottom-0 before:-right-10">
-              <img src={Porfile} className="rounded-image relative z-10 w-[280px] m-auto sm:w-[600px]" />
+                <img
+                  src={Porfile}
+                  className="rounded-image relative z-10 w-[280px] m-auto sm:w-[600px]"
+                />
               </div>
             </div>
           </div>
@@ -87,36 +101,49 @@ function App() {
             <div className="flex flex-col sm:flex-row gap-10 mt-11">
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={Project1} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">
-                  Furniture store landing page
-                </h3>
+                <h3 className="text-2xl font-semibold mt-8">Stockkeep</h3>
                 <p className="text-gray-400 text-sm mt-2">
-                  Responsive HTML/CSS layout for online furniture store. HTML5,
-                  CSS3 (SCSS)
+                  StockKeep, crafted with Flutter for the frontend and Django
+                  for the backend, seamlessly manages inventory with real-time
+                  sync, insightful stock tracking, and advanced analytics,
+                  empowering businesses with efficient operations and informed
+                  decision-making.
                 </p>
                 <div className="flex mt-12 gap-2">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
-                    Live preview
-                  </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                  <button
+                    className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500
+                  "
+                    onClick={() =>
+                      window.open(
+                        "https://github.com/yousef391/stockeep",
+                        "_blank"
+                      )
+                    }
+                  >
                     Checkout github
                   </button>
                 </div>
               </div>
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={Project2} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">
-                  Designer furniture store landing page
-                </h3>
+                <h3 className="text-2xl font-semibold mt-8">Hiking</h3>
                 <p className="text-gray-400 text-sm mt-2">
-                  Responsive HTML/CSS layout for online store of designer
-                  furniture. HTML5, CSS3 (SCSS)
+                  "Hiking" lets you explore the outdoors with ease. Developed
+                  with Flutter and Django, Hiking enables you to find hiking
+                  groups in your location effortlessly, connecting you with
+                  like-minded adventurers for unforgettable experiences on the
+                  trails.
                 </p>
                 <div className="flex gap-2 mt-12">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
-                    Live preview
-                  </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                  <button
+                    className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500"
+                    onClick={() =>
+                      window.open(
+                        "https://github.com/yousef391/hikingapp",
+                        "_blank"
+                      )
+                    }
+                  >
                     Checkout github
                   </button>
                 </div>
@@ -125,39 +152,56 @@ function App() {
             <div className="flex flex-col sm:flex-row  gap-10 mt-11">
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={Project3} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">
-                  Landing page for front-end developer
-                </h3>
+                <h3 className="text-2xl font-semibold mt-8">Needy</h3>
                 <p className="text-gray-400 text-sm mt-2">
-                  Responsive HTML/CSS layout for landing page for front-end
-                  developer. HTML5, CSS3 (SCSS)
+                  "Needy App" is the ultimate platform for fostering community
+                  generosity. Developed with Flutter and Firebase, Needy
+                  empowers users to easily offer or find items they need,
+                  promoting a culture of sharing and support. Whether it's
+                  books, clothes, or household items, Needy connects people who
+                  want to give with those who are in need, creating a vibrant
+                  community where everyone can benefit.
                 </p>
                 <div className="flex gap-4 mt-12">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
-                    Live preview
-                  </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                  <button
+                    className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500"
+                    onClick={() =>
+                      window.open(
+                        "https://github.com/yousef391/Bloodapp",
+                        "_blank"
+                      )
+                    }
+                  >
                     Checkout github
                   </button>
                 </div>
               </div>
               <div className="border border-gray-500 rounded-md p-5 flex-1">
-                <img src={Project4} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">
-                  Website redesign for The Venus project
-                </h3>
+                {" "}
+                <img src={Project4} className="w-full h-auto" />{" "}
+                <h3 className="text-2xl font-semibold mt-8"> BloodApp </h3>{" "}
                 <p className="text-gray-400 text-sm mt-2">
-                  Responsive HTML/CSS layout for The Venus project. HTML5, CSS3
-                  (SCSS)
-                </p>
+                  {" "}
+                  BloodApp: Your lifeline in times of need. Developed with
+                  Flutter and Firebase, BloodApp seamlessly connects blood
+                  donors with those in need, ensuring a swift and efficient
+                  blood donation process.{" "}
+                </p>{" "}
                 <div className="flex gap-2 mt-12">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
-                    Live preview
-                  </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
-                    Checkout github
-                  </button>
-                </div>
+                  {" "}
+                  <button
+                    className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500"
+                    onClick={() =>
+                      window.open(
+                        "https://github.com/yousef391/Bloodapp",
+                        "_blank"
+                      )
+                    }
+                  >
+                    {" "}
+                    Checkout github{" "}
+                  </button>{" "}
+                </div>{" "}
               </div>
             </div>
           </div>
@@ -181,8 +225,7 @@ function App() {
                 </div>
                 <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
               </div>
-             
-              
+
               <div className="mt-8">
                 <div className="flex justify-between items-center">
                   <h2 className="font-semibold">Python</h2>
@@ -190,7 +233,7 @@ function App() {
                 </div>
                 <span className="w-[50%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
               </div>
-            
+
               <div className="mt-8">
                 <div className="flex justify-between items-center">
                   <h2 className="font-semibold">Django</h2>
@@ -202,11 +245,14 @@ function App() {
           </div>
         </section>
         {/* Additional skills section */}
-        
-        
       </main>
       <footer>
         <div className="container m-auto flex justify-between px-4 py-6">
+          <div>
+            <p className="text-gray-300 text-sm">
+              Contact : yf.ouddane@esi-sba.dz
+            </p>
+          </div>
           <div>
             <p className="text-gray-300 text-sm">Copyright @ 2023</p>
           </div>
@@ -231,15 +277,16 @@ function App() {
           </div>
         </div>
       </footer>
-      {
-        scrolling && (
-          <button className="fixed block right-8 bottom-0 w-24" onClick={() => {
-            window.scrollTo(0,0);
-          }}>
-            <img src={ArrowDown} />
-          </button>
-        )
-      }
+      {scrolling && (
+        <button
+          className="fixed block right-8 bottom-0 w-24"
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={ArrowDown} />
+        </button>
+      )}
     </div>
   );
 }

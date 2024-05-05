894541a6e3b21a391d315bb2b0090bfbaf383e2d
yousrf391 <yousefouddane3@gmail.com>
2024-05-05 11:12:15 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Porfile from "./assets/yousef.jpg";
+import Porfile from "./assets/profile.jpg";
 import Project1 from "./assets/stockeep.png";
 import Project2 from "./assets/hiking.png";
 import Project3 from "./assets/needyy.png";
@@ -88,7 +88,7 @@ function App() {
               <div className="after:bg-[url('./large-long.png')] after:bg-contain after:block after:bg-no-repeat after:w-[420px] after:h-[320px] after:absolute after:top-0 after:-left-20 sm:after:-left-40 before:bg-[url('./small.png')] before:bg-contain before:block before:bg-no-repeat before:w-[220px] before:h-[220px] before:absolute before:bottom-0 before:-right-10">
                 <img
                   src={Porfile}
-                  className="rounded-image relative z-10 w-[280px] m-auto sm:w-[600px]"
+                  className="rounded-image relative z-10 w-[280px] m-auto sm:w-[400px]"
                 />
               </div>
             </div>

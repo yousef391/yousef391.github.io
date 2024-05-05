80f352ee75428a191f7a4f6144c1a41fd5f84421
yousrf391 <yousefouddane3@gmail.com>
2024-05-05 10:51:27 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Porfile from "./assets/yousef.jpg";
 import Project1 from "./assets/stockeep.png";
 import Project2 from "./assets/hiking.png";
-import Project3 from "./assets/fitted.png";
+import Project3 from "./assets/needyy.png";
 import Project4 from "./assets/BLoodAPP.png";
 import Facebook from "./assets/facebook.svg";
 import LinkedIn from "./assets/linkedin.svg";
@@ -241,6 +241,14 @@ function App() {
                 </div>
                 <span className="w-[45%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
               </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">Java</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[30%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+
             </div>
           </div>
         </section>
